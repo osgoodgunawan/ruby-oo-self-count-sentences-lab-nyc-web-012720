311b7004d00ed1062a189e5f080c9e39e544f8c9
osgoodgunawan <ogunawan@ucsd.edu>
2020-01-31 10:07:45 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,38 @@ require 'pry'
 class String
 
   def sentence?
-    
+      if self.include?(".")
+    return true
+      else 
+        return false
+      end
   end
 
   def question?
+    if self.include?("?")
+      return true
+    else 
+      return false 
+    end
 
   end
 
   def exclamation?
-
+    if self.include?("!")
+      return true
+    else
+      return false
+    end 
   end
 
   def count_sentences
-
+  count=0
+    self.split(" ").each do |word|
+      if word.include?(".")|| word.include?("?")||word.include?("!")
+        count+=1
+      end
+  
+      end
+    count
   end
 end
